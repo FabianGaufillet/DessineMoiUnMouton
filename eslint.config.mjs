dec212a7c3350b4e5831c5a53baa6c4af326b0ca
Fabian Gaufillet <fabian.gaufillet@gmail.com>
2024-09-14 11:57:23 +0200

    Initialisation du projet

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import eslintConfigPrettier from "eslint-config-prettier";
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+  eslintConfigPrettier,
+];
