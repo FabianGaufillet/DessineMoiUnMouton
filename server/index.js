8dffb4371e2f72f44b8230675daaaf1b149f8142
Fabian Gaufillet <fabian.gaufillet@gmail.com>
2024-09-22 17:16:49 +0200

    Back-end : envoi des cookies au navigateur

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import { router as user } from "./routes/user.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:4200", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
