f0de57cd0e0c3871d0f4cebc567f7745d97e231d
git user <user@gmail.com>
2024-09-23 15:38:35 +0200

    Back-end : Ajout des websockets

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
 import { SERVER_PORT, MONGODB_URI } from "./config/index.js";
 import { router as user } from "./routes/user.js";
 
@@ -23,7 +24,7 @@ const start = async () => {
       res.status(error.status).json(error.message);
     });
 
-    app.listen({ port: SERVER_PORT }, () => {
+    return app.listen({ port: SERVER_PORT }, () => {
       console.log(`Server started on port ${SERVER_PORT}`);
     });
   } catch (err) {
@@ -31,4 +32,12 @@ const start = async () => {
   }
 };
 
-start();
+(async () => {
+  const server = await start();
+  const io = new Server(server, {
+    cors: { origin: "http://localhost:4200" },
+  });
+  io.on("connection", (socket) => {
+    socket.on("chat", (data) => io.emit("chat", data));
+  });
+})();
